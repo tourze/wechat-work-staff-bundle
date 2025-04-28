d9cbd478395dbbd89fa0975cf84fba64aa829dac
github-ci <you@example.com>
2025-04-28 09:46:11 +0000

    fix: 修复字段错误

--- src/Entity/AgentUser.php
+++ src/Entity/AgentUser.php
@@ -38,7 +38,7 @@ class AgentUser
     #[ORM\GeneratedValue(strategy: 'CUSTOM')]
     #[ORM\CustomIdGenerator(SnowflakeIdGenerator::class)]
     #[ORM\Column(type: Types::BIGINT, nullable: false, options: ['comment' => 'ID'])]
-    private ?string $id = '0';
+    private ?string $id = null;
 
     #[Ignore]
     #[ORM\ManyToOne(targetEntity: Agent::class)]
